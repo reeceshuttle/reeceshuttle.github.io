abf0d8b49ee3199f8261daed5ff4d1b40fe712a3
Reece Shuttleworth <reece.shuttle@gmail.com>
2024-02-01 22:42:09 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-// const landing = document.querySelector(".landing-page");
-
-// ocument.body.addEventListener("click", function (evt) {
-//   console.dir(this);
-//   //note evt.target can be a nested element, not the body element, resulting in misfires
-//   console.log(evt.target);
-//   alert("body clicked");
-// });
-
 const landing = document.querySelector(".landing-page");
 
 document.addEventListener("click", function (event) {
